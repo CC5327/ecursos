913adb6dd609e9f22831b6cee1c62b40d1733521
Eduardo <eriveros@dcc.uchile.cl>
2020-06-12 00:50:51 -0400

    adding query.php

---
+++ query.php
@@ -0,0 +1,26 @@
+<?php
+require_once("config.php");
+$connstr = "host=$DB_HOST port=$DB_PORT dbname=$DB_NAME user=$DB_USER password=$DB_PASS";
+$conn = pg_pconnect($connstr);
+if (isset($_POST["name"])){
+        $query = pg_query($conn, "UPDATE users SET name='$_POST["name"]' WHERE id = 1");
+    }
+$nombre = pg_query($conn, "SELECT name FROM users WHERE id = 1");
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualizar tu nombre de perfil</title>
+</head>
+<body>
+    <p>Ahora mismo tu nombre de perfil es <?= $nombre ?>, ¿deseas cambiarlo?</p>
+    <form action="./" method="post">
+    <input type="text" name="name" id="name" placeholder="nuevo nombre">
+    <input type="submit" value="Cambiar">
+    </form>
+</body>
+</html>
